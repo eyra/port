ab784de0740581cc18678e68349113e7a6f9c420
emielvdveen <emiel@applike.nl>
2022-09-23 21:56:32 +0200

    Added React app using Typescript, Tailwind, ts-standard, husky

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App (): any {
+  return (
+    <h1 className='text-3xl font-bold underline text-blue-500'>
+      Simple React Typescript Tailwind Samples
+    </h1>
+  )
+};
+
+export default App
