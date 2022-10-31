9caeae2dd25806f2abad477ad4b56da11a2782cd
emielvdveen <emiel@applike.nl>
2022-10-31 10:37:42 +0100

    Added support for consent table pagination

--- src/framework/visualisation/react/ui/elements/button.tsx
+++ src/framework/visualisation/react/ui/elements/button.tsx
@@ -1,6 +1,7 @@
 import { Weak } from '../../../../helpers'
-import { PropsUIButtonForward, PropsUIButtonLabel, PropsUIButtonPrimary, PropsUIButtonSecundary } from '../../../../types/elements'
+import { PropsUIButtonBack, PropsUIButtonForward, PropsUIButtonLabel, PropsUIButtonPrimary, PropsUIButtonSecundary } from '../../../../types/elements'
 
+import BackSvg from '../../../../../assets/images/back.svg'
 import ForwardSvg from '../../../../../assets/images/forward.svg'
 
 export const PrimaryButton = ({ label, color = 'bg-primary text-white', onClick }: Weak<PropsUIButtonPrimary>): JSX.Element => {
@@ -23,6 +24,25 @@ export const SecondaryButton = ({ label, color = 'bg-delete text-delete', onClic
   )
 }
 
+export const BackButton = ({ label, onClick }: Weak<PropsUIButtonBack>): JSX.Element => {
+  return (
+    <div className='pt-1 pb-1 active:pt-5px active:pb-3px rounded bg-opacity-0 focus:outline-none cursor-pointer ' onClick={onClick}>
+      <div className='flex items-center'>
+        <div>
+          <img className='mr-2 -mt-2px' src={BackSvg} alt={label} />
+        </div>
+        <div className='focus:outline-none'>
+          <div className='flex flex-col justify-center h-full items-center'>
+            <div className='flex-wrap text-button font-button text-grey1'>
+              {label}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export const ForwardButton = ({ label, onClick }: Weak<PropsUIButtonForward>): JSX.Element => {
   return (
     <div className='pt-1 pb-1 active:pt-5px active:pb-3px rounded bg-opacity-0 focus:outline-none cursor-pointer ' onClick={onClick}>
@@ -35,16 +55,16 @@ export const ForwardButton = ({ label, onClick }: Weak<PropsUIButtonForward>): J
           </div>
         </div>
         <div>
-          <img className='ml-4 -mt-2px' src={ForwardSvg} alt={label} />
+          <img className='ml-2 -mt-2px' src={ForwardSvg} alt={label} />
         </div>
       </div>
     </div>
   )
 }
 
-export const LabelButton = ({ label, onClick }: Weak<PropsUIButtonLabel>): JSX.Element => {
+export const LabelButton = ({ label, color = 'text-grey1', onClick }: Weak<PropsUIButtonLabel>): JSX.Element => {
   return (
-    <div className='pt-15px pb-15px active:shadow-top4px active:pt-4 active:pb-14px leading-none font-button text-button rounded pr-4 pl-4 cursor-pointer bg-opacity-0' onClick={onClick}>
+    <div className={`pt-15px pb-15px active:shadow-top4px active:pt-4 active:pb-14px leading-none font-button text-button rounded pr-4 pl-4 cursor-pointer bg-opacity-0 ${color}`} onClick={onClick}>
       <div id='confirm-button' className='flex-wrap'>
         {label}
       </div>
