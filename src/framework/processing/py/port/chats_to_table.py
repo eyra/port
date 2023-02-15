e7e23a8a5f553d0a91f715915a8706364ed14c75
trbKnl <ncdeschipper@gmail.com>
2023-02-15 20:41:22 +0100

    first real version of chat processing done, I think this could be tested

--- src/framework/processing/py/port/chats_to_table.py
+++ src/framework/processing/py/port/chats_to_table.py
@@ -1,74 +1,81 @@
 import re
+from typing import Tuple
+import unicodedata
+
 import logging 
+
+import pandas as pd
+
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 SIMPLIFIED_REGEXES = [
-    r'^%m/%d/%y, %H:%M - %name: %chat_message$',
-    r'^\[%d/%m/%y, %H:%M:%S\] %name: %chat_message$',
-    r'^%d-%m-%y %H:%M - %name: %chat_message$',
-    r'^\[%d-%m-%y %H:%M:%S\] %name: %chat_message$',
-    r'^\[%m/%d/%y, %H:%M:%S\] %name: %chat_message$',
-    r'^%d/%m/%y, %H:%M – %name: %chat_message$',
-    r'^%d/%m/%y, %H:%M - %name: %chat_message$',
-    r'^%d.%m.%y, %H:%M – %name: %chat_message$',
-    r'^%d.%m.%y, %H:%M - %name: %chat_message$',
-    r'^%m.%d.%y, %H:%M - %name: %chat_message$',
-    r'^%m.%d.%y %H:%M - %name: %chat_message$',
-    r'^\[%d/%m/%y, %H:%M:%S %P\] %name: %chat_message$',
-    r'^\[%m/%d/%y, %H:%M:%S %P\] %name: %chat_message$',
-    r'^\[%d.%m.%y, %H:%M:%S\] %name: %chat_message$',
-    r'^\[%m/%d/%y %H:%M:%S\] %name: %chat_message$',
-    r'^\[%m-%d-%y, %H:%M:%S\] %name: %chat_message$',
-    r'^\[%m-%d-%y %H:%M:%S\] %name: %chat_message$',
-    r'^%m-%d-%y %H:%M - %name: %chat_message$',
-    r'^%m-%d-%y, %H:%M - %name: %chat_message$',
-    r'^%m-%d-%y, %H:%M , %name: %chat_message$',
-    r'^%m/%d/%y, %H:%M , %name: %chat_message$', 
-    r'^%d-%m-%y, %H:%M , %name: %chat_message$',
-    r'^%d/%m/%y, %H:%M , %name: %chat_message$',
-    r'^%d.%m.%y %H:%M – %name: %chat_message$', 
-    r'^%m.%d.%y, %H:%M – %name: %chat_message$',
-    r'^%m.%d.%y %H:%M – %name: %chat_message$',
-    r'^\[%d.%m.%y %H:%M:%S\] %name: %chat_message$',
-    r'^\[%m.%d.%y, %H:%M:%S\] %name: %chat_message$',
-    r'^\[%m.%d.%y %H:%M:%S\] %name: %chat_message$'
+    r"^%m/%d/%y, %H:%M - %name: %chat_message$",
+    r"^\[%d/%m/%y, %H:%M:%S\] %name: %chat_message$",
+    r"^%d-%m-%y %H:%M - %name: %chat_message$",
+    r"^\[%d-%m-%y %H:%M:%S\] %name: %chat_message$",
+    r"^\[%m/%d/%y, %H:%M:%S\] %name: %chat_message$",
+    r"^%d/%m/%y, %H:%M – %name: %chat_message$",
+    r"^%d/%m/%y, %H:%M - %name: %chat_message$",
+    r"^%d.%m.%y, %H:%M – %name: %chat_message$",
+    r"^%d.%m.%y, %H:%M - %name: %chat_message$",
+    r"^%m.%d.%y, %H:%M - %name: %chat_message$",
+    r"^%m.%d.%y %H:%M - %name: %chat_message$",
+    r"^\[%d/%m/%y, %H:%M:%S %P\] %name: %chat_message$",
+    r"^\[%m/%d/%y, %H:%M:%S %P\] %name: %chat_message$",
+    r"^\[%d.%m.%y, %H:%M:%S\] %name: %chat_message$",
+    r"^\[%m/%d/%y %H:%M:%S\] %name: %chat_message$",
+    r"^\[%m-%d-%y, %H:%M:%S\] %name: %chat_message$",
+    r"^\[%m-%d-%y %H:%M:%S\] %name: %chat_message$",
+    r"^%m-%d-%y %H:%M - %name: %chat_message$",
+    r"^%m-%d-%y, %H:%M - %name: %chat_message$",
+    r"^%m-%d-%y, %H:%M , %name: %chat_message$",
+    r"^%m/%d/%y, %H:%M , %name: %chat_message$", 
+    r"^%d-%m-%y, %H:%M , %name: %chat_message$",
+    r"^%d/%m/%y, %H:%M , %name: %chat_message$",
+    r"^%d.%m.%y %H:%M – %name: %chat_message$", 
+    r"^%m.%d.%y, %H:%M – %name: %chat_message$",
+    r"^%m.%d.%y %H:%M – %name: %chat_message$",
+    r"^\[%d.%m.%y %H:%M:%S\] %name: %chat_message$",
+    r"^\[%m.%d.%y, %H:%M:%S\] %name: %chat_message$",
+    r"^\[%m.%d.%y %H:%M:%S\] %name: %chat_message$",
+    r"^(?P<year>.*?)(?:\] | - )%name: %chat_message$"  # Fallback catch all regex
 ]
 
 
 REGEX_CODES = {
-    '%Y': r'(?P<year>\d{2,4})',
-    '%y': r'(?P<year>\d{2,4})',
-    '%m': r'(?P<month>\d{1,2})',
-    '%d': r'(?P<day>\d{1,2})',
-    '%H': r'(?P<hour>\d{1,2})',
-    '%I': r'(?P<hour>\d{1,2})',
-    '%M': r'(?P<minutes>\d{2})',
-    '%S': r'(?P<seconds>\d{2})',
-    '%P': r'(?P<ampm>[AaPp].? ?[Mm].?)',
-    '%p': r'(?P<ampm>[AaPp].? ?[Mm].?)',
-    '%name': r'(?P<name>[^:]*)',
-    '%chat_message': r'(?P<chat_message>.*)'
+    "%Y": r"(?P<year>\d{2,4})",
+    "%y": r"(?P<year>\d{2,4})",
+    "%m": r"(?P<month>\d{1,2})",
+    "%d": r"(?P<day>\d{1,2})",
+    "%H": r"(?P<hour>\d{1,2})",
+    "%I": r"(?P<hour>\d{1,2})",
+    "%M": r"(?P<minutes>\d{2})",
+    "%S": r"(?P<seconds>\d{2})",
+    "%P": r"(?P<ampm>[AaPp].? ?[Mm].?)",
+    "%p": r"(?P<ampm>[AaPp].? ?[Mm].?)",
+    "%name": r"(?P<name>[^:]*)",
+    "%chat_message": r"(?P<chat_message>.*)"
 }
 
     
 def generate_regexes(simplified_regexes):
     """
-    Create the final regular expression by substituting REGEX_CODES
-    into SIMPLIFIED_REGEXES
-    """
+    Create the complete regular expression by substituting 
+    REGEX_CODES into SIMPLIFIED_REGEXES
 
+    """
     final_regexes = []
 
     for simplified_regex in simplified_regexes:
 
-        codes = re.findall(r'\%\w*', simplified_regex)
+        codes = re.findall(r"\%\w*", simplified_regex)
         for code in codes:
             try:
                 simplified_regex = simplified_regex.replace(code, REGEX_CODES[code])
             except KeyError:
-                logging.error(f"Could find regular expression for : {code}")
+                logging.error(f"Could not find regular expression for: {code}")
 
         final_regexes.append(simplified_regex)
 
@@ -78,8 +85,57 @@ def generate_regexes(simplified_regexes):
 REGEXES =  generate_regexes(SIMPLIFIED_REGEXES)
 
 
+def remove_unwanted_characters(s: str) -> str:
+    """
+    Cleans string from bytes using magic
 
-def determine_regex_from_chat(lines):
+    Keeps empjis intact
+    """
+    s = "".join(ch for ch in s if unicodedata.category(ch)[0]!="C")
+    s = unicodedata.normalize("NFKD", s)
+    return s
+
+
+def create_data_point_from_chat(chat: str, regex) -> dict[str, str]:
+    """
+    Construct data point from chat messages
+    """
+    result = re.match(regex, chat)
+    if result:
+        result = result.groupdict()
+    else:
+        result = {}
+
+    # Construct data 
+    date = '-'.join([result.get("year", ""), result.get("month", ""), result.get("day", "")])
+    name = result.get("name", "")
+    chat_message = result.get("chat_message", "")
+
+    return {"date": date, "name": name, "chat_message": chat_message}
+
+
+def remove_empty_chats(df: pd.DataFrame) -> None:
+    """
+    Removes all rows from the chat dataframe where no regex matched
+    """
+    df.drop(df[df.chat_message == ""].index, inplace=True)
+
+
+def extract_users(df: pd.DataFrame) -> list[str]:
+    """
+    Extracts unique usersnames from chat dataframe
+    """
+    return list(set(df["name"]))
+
+
+def filter_username(df: pd.DataFrame, username: str) -> None:
+    """
+    Extracts unique usersnames from chat dataframe
+    """
+    df.drop(df[df.name != username].index, inplace=True)
+    
+
+def determine_regex_from_chat(lines: list[str]) -> str:
     """
     Read lines of chat return the first regex that matches
     That regex is used to process the chatfile
@@ -88,20 +144,22 @@ def determine_regex_from_chat(lines):
     length_lines = len(lines)
     for index, line in enumerate(lines):
         for regex in REGEXES:
-
             if re.match(regex, line):
                 logging.debug(f"Matched regex: {regex}")
                 return regex
 
-            if index > length_lines or index > 100:
-                logging.error(f"No matching regex found: {regex}")
-                return None
+        if index > length_lines:
+            break
+
+    logging.error(f"No matching regex found:")
+    raise Exception(f"No matching regex found")
 
 
-def construct_message(current_line, next_line, regex):
+
+def construct_message(current_line: str, next_line: str, regex: str) -> Tuple[bool, str]:
     """
-    Helper function: determines whether the next line matches the regex
-    if not than the line belongs to the message on the current line
+    Helper function: determines whether the next line in the chat matches the regex
+    in case of no match it means that the line belongs to the message on the current line
     """
 
     match_next_line = re.match(regex, next_line)
@@ -113,49 +171,68 @@ def construct_message(current_line, next_line, regex):
         return False, current_line
 
 
+def read_chat_file(path_to_chat_file: str) -> list[str]:
+    try:
+        with open(path_to_chat_file) as f:
+            lines = f.readlines()
+            lines = [remove_unwanted_characters(line) for line in lines]
+            return lines
 
-def parse_chat(chat_file):
+    except Exception as e:
+        raise e
+
+
+def parse_chat(path_to_chat: str) -> pd.DataFrame:
     """
-    FUNCTION NOTE DONE
-    SPLIT OPENING FROM READING
+    Read chat from file, parse, return df
 
-    Read in chat process lines
+    In case of error returns empty df
     """
     out = []
-    with open(chat_file) as f:
-        lines = f.readlines()
-        ## Remove all bytes, this also removes emoji's tweak later
-        #lines = [re.sub(r'[^\x00-\x7F]+','', line) for line in lines]
 
-    regex = determine_regex_from_chat(lines)
+    try:
+        lines = read_chat_file(path_to_chat)
+        regex = determine_regex_from_chat(lines)
 
-    current_line = lines.pop(0)
-    next_line = lines.pop(0)
+        current_line = lines.pop(0)
+        next_line = lines.pop(0)
 
-    while True:
-        try:
-            match_next_line, message = construct_message(current_line, next_line, regex)
+        while True:
+            try:
+                match_next_line, chat = construct_message(current_line, next_line, regex)
 
-            while not match_next_line:
+                while not match_next_line:
+                    next_line = lines.pop(0)
+                    match_next_line, chat = construct_message(chat, next_line, regex)
+
+                data_point = create_data_point_from_chat(chat, regex)
+                out.append(data_point)
+
+                current_line = next_line
                 next_line = lines.pop(0)
-                match_next_line, message = construct_message(message, next_line, regex)
 
-            # STILL TO IMPLEMENT CAPTURE GROUPS HERE
-            # THEY DONT PLAY NICE WITH POSSIBLE FALLBACK REGEX
-            out.append(message)
+            # IndexError occurs when pop fails
+            # Meaning we processed all chat messages
+            except IndexError:
+                data_point = create_data_point_from_chat(current_line, regex)
+                out.append(data_point)
+                break
+    except Exception as e:
+        logging.error(e)
 
-            current_line = next_line
-            next_line = lines.pop(0)
-
-        except IndexError:
-            out.append(current_line)
-            break
-
-    return out
+    finally:
+        return pd.DataFrame(out)
 
 
-
+##### Try it out!
 
 PATH_TO_CHAT = "/home/turbo/Downloads/WhatsApp-chat met Niek De Schipper.txt"
 PATH_TO_CHAT = "/home/turbo/ddp-inspector/example_ddps/whatsapp/whatsapp_bojan/_chat.txt"        
-parse_chat(PATH_TO_CHAT)
+df = parse_chat(PATH_TO_CHAT)
+remove_empty_chats(df)
+extract_users(df)
+
+df
+
+
+
