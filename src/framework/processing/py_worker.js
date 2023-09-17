eff29884655b0fbb6fa28e0bdd9bb8768391f9eb
Jeroen <jeroen@adaptiveconstant.com>
2023-09-17 10:07:18 +0200

    Made loading of Python script use relative path
    This ensures it loads when the HTML is loaded from a sub-folder.

--- src/framework/processing/py_worker.js
+++ src/framework/processing/py_worker.js
@@ -95,7 +95,7 @@ function installPortPackage() {
   console.log('[ProcessingWorker] load port package')
   return self.pyodide.runPythonAsync(`
     import micropip
-    await micropip.install("/port-0.0.0-py3-none-any.whl", deps=False)
+    await micropip.install("../../port-0.0.0-py3-none-any.whl", deps=False)
     import port
   `);  
 }
